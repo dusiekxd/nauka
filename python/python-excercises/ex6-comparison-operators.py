c522855a54c3b21602c34c11482ffa59b0607ea2
dusiekxd <amadeusz.niemcewicz@gmail.com>
2021-10-05 18:00:56 +0200

    adding few more excercises in python

---
+++ python/python-excercises/ex6-comparison-operators.py
@@ -0,0 +1,18 @@
+a = 5               
+b = 5
+
+# print(a == 5)       # a is equal 5
+# print(a != 5)       # a is different than 5
+
+# print (a > 5)       # a is bigger than 5
+# print (a < 5)       # a is lesser than 5
+
+# print (a >= 5)      # a is bigger or equal 5
+# print (a <= 5)      # a is lesser or equal 5
+
+
+#result type - bool (true/false)
+
+ 
+print (a == b)
+
